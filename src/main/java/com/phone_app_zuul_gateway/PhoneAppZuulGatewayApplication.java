b064818b44ef43a6467f6aeed5feb273b040fbb3
bsyso <bsyso@softserveinc.com>
2022-12-30 11:43:41 +0100

    Initial commit

---
+++ src/main/java/com/phone_app_zuul_gateway/PhoneAppZuulGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.phone_app_zuul_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneAppZuulGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhoneAppZuulGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/phone_app_zuul_gateway/PhoneAppZuulGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phone_app_zuul_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneAppZuulGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
